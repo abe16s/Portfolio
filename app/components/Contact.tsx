f92938f7748750f23343737c4bc603c77f96e383
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-11-14 15:32:23 +0300

    Update resume link

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -72,7 +72,7 @@ const Contact = () => {
                         <li><a className="flex gap-2 items-baseline" href="https://maps.app.goo.gl/G7qLqXABwttEgCfZA" target="_blank"><FaHouse/><span className="hover:text-blue-500">King George VI St, Addis Ababa, Ethiopia</span></a></li>
                         <li><a className="flex gap-2 items-baseline" href="tel:+251936120470"><FaPhone/> <span className="hover:text-blue-500">+251936120470</span></a></li>
                         <li><a className="flex gap-2 items-baseline" href="mailto:abenezerseifu123@gmail.com"><FaEnvelope/> <span className="hover:text-blue-500">abenezerseifu123@gmail.com</span></a></li>
-                        <li><a className="flex gap-2 items-baseline" href="https://drive.google.com/file/d/1Zs8IyE53s1rx0sAW9u7DU506uA9LEFcP/view?usp=sharing" target="_blank"><FaFile/><span className="hover:text-blue-500">Resume</span></a></li>
+                        <li><a className="flex gap-2 items-baseline" href="https://drive.google.com/file/d/1W_xrPx0gxsqU3cV0XAl6V9adj9y4SYNl/view?usp=sharing" target="_blank"><FaFile/><span className="hover:text-blue-500">Resume</span></a></li>
                     </ul>
                 </div>
                 <form className="p-3 flex flex-col text-black w-full max-w-[500px]"
