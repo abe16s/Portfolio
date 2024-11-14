e0dae700543b4beaa213a33623ee0370184459ba
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-11-14 15:28:27 +0300

    Changed link to insurance company project's link

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
     { title: 'Local Idir Managing App', image: '/Idir.png', description: 'Idir managing application that helps facilitate the management of Idir members, monthly payments and other financial workings of Idir - Desktop App (Java Swing)', link: 'https://github.com/abe16s/IDIR' },
     { title: 'SkillTrade Web', image: '/SkillTradeWeb.png', description: 'A hub that is used to connect skilled technicians with customers easily - Web App', link: 'https://github.com/abe16s/SkillTrade' },
     { title: 'SkillTrade Mobile', image: '/SkillTradeMobile.jpg', description: 'A hub that is used to connect skilled technicians with customers easily - Mobile App (Flutter)', link: 'https://github.com/bexeGrace/flutter-2024-proj-SkillTrade-Bloc' },
-    { title: 'User portal for insurance companies', image: '/Insurance.png', description: 'A portal that helps insurance customers manage their insurance easily from anywhere - Web App (Next.js)', link: 'https://github.com/Tamiru-Alemnew/insurance-company-system/tree/main/insurance' },
+    { title: 'User portal for insurance companies', image: '/Insurance.png', description: 'A portal that helps insurance customers manage their insurance easily from anywhere - Web App (Next.js)', link: 'https://github.com/Tamiru-Alemnew/Insurance-company-system' },
     { title: 'Task Manager API', image: 'https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg', description: 'A back-end API project using golang and gin, with full CRUD operations, database integration and authentication & authorization', link: 'https://github.com/abe16s/Task-Manager-API-Final' },
   ];
 
